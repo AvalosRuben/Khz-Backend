4e726331aa68f1f16aea4bd57823a9a74c33983d
AvalosRuben <rubenavalosmtz@gmail.com>
2026-07-06 10:48:38 -0600

    feat(Models): Added the station model

---
+++ models/station.go
@@ -0,0 +1,8 @@
+type Station struct{
+	Id string `gorm:"type:uuid;primaryKey"`
+	Frequency float64 `gorm:"column:name;unique"`
+	Name string `gorm:"column:name"`
+	Genre string `gorm:"column:genre"`
+	Stream_Url string `gorm:"column:stream_url"`
+	Created_At time.Time `gorm:"column:created_at"`
+}
